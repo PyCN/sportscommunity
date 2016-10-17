001d8b0a9149c0434420bcb2a4aa5e46928d8b7d
ZivLi <lijiaxing@newborn-town.com>
2016-10-17 09:32:11 +0800

    config files

---
+++ config.py
@@ -0,0 +1,22 @@
+import os
+import sys
+import _env    # noqa
+import redis
+from pymongo import Connection
+
+__author__ = 'ZivLi'
+
+debug = True
+
+
+PREFIX = os.path.abspath(
+    os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), ''
+    )
+)
+if PREFIX not in sys.path:
+    sys.path.append(PREFIX)
+
+MONGO = dict(host='mongodb://mongo:27017')
+
+redis_recom = redis.Redis(host='redis', port='6379', db=4)
---
+++ error_code.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+
+ERROR_CODE = {
+    'argument_error': {
+        "error_code": "10000",
+        "error": u"miss key argument(s)"
+    },
+    "unexpect_argument": {
+        "error_code": "10001",
+        "error": u"unexpect argument(s)"
+    },
+    "click_error": {
+        "error_code": "20000",
+        "error": u"we can't recond the click"
+    },
+    "key_not_exist": {
+        "error_code": 30001,
+        "error": u"key not exist"
+    },
+    "install_error": {
+        "error_code": "30000",
+        "error": u"we can't recond the instll"
+    },
+    "get_dates_error": {
+        "error_code": 40001,
+        "error": u"get native dates error"
+    },
+    "user_unauthorized": {
+        "error_code": 40002,
+        "error": u"user not Unauthorized"
+    },
+    "sid_error": {
+        "error_code": 50001,
+        "error": "illegal sid"
+
+    }
+
+}
